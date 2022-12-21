1e6efa9f646de68f6ec185af4d1f3f72468d2b84
Md. Taifuzzaman Bilash <bilashism@gmail.com>
2022-12-22 01:09:18 +0600

    added pagination for all services

--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -3,18 +3,26 @@ import useTitle from "../../hooks/useTitle";
 import Service from "../../components/Service";
 import { useQuery } from "@tanstack/react-query";
 import LoadingCircle from "../../components/ui/LoadingCircle";
+import { useState } from "react";
 
 const Services = () => {
   useTitle("Services");
-
-  const { data: services = [], isLoading } = useQuery({
-    queryKey: ["services"],
-    queryFn: () =>
-      fetch(`${import.meta.env.VITE_APP_SERVER}/services`)
-        .then(res => res.json())
-        .then(data => data)
-        .catch(err => console.error(err))
-  });
+  const [page, setPage] = useState(0);
+  const itemsPerPage = 6;
+  const { data: { count = 0, result: services = [] } = {}, isLoading } =
+    useQuery({
+      queryKey: ["services", page, itemsPerPage],
+      queryFn: () =>
+        fetch(
+          `${
+            import.meta.env.VITE_APP_SERVER
+          }/services?page=${page}&itemsPerPage=${itemsPerPage}`
+        )
+          .then(res => res.json())
+          .then(data => data)
+          .catch(err => console.error(err))
+    });
+  const pageNumber = Math.ceil(count / itemsPerPage);
 
   return (
     <main>
@@ -30,6 +38,20 @@ const Services = () => {
         <div className="grid gap-8 lg:grid-cols-3">
           {services.map(service => (
             <Service key={service._id} service={service} />
+          ))}
+        </div>
+        <div className="flex flex-wrap gap-4 mt-12 justify-center">
+          {[...Array(pageNumber).keys()].map(num => (
+            <button
+              key={`page-${num}`}
+              aria-label={`load page no ${num + 1}`}
+              type="button"
+              className={`${
+                page === num ? "bg-green-300" : "bg-slate-300"
+              } p-2 rounded font-bold w-10 h-10 aspect-square`}
+              onClick={() => setPage(num)}>
+              {num + 1}
+            </button>
           ))}
         </div>
       </div>
