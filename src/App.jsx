c08a1cc4b886b553c93c1233eaaade4f7e4bf485
Md. Taifuzzaman Bilash <bilashism@gmail.com>
2022-12-21 18:46:10 +0600

    setup react query & added loading screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/router";
 
+const queryClient = new QueryClient();
 function App() {
   return (
-    <div className="max-w-screen-xl mx-auto px-4">
-      <RouterProvider router={router}></RouterProvider>
-      <Toaster />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="max-w-screen-xl mx-auto px-4">
+        <RouterProvider router={router}></RouterProvider>
+        <Toaster />
+      </div>
+    </QueryClientProvider>
   );
 }
 
--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -1,12 +1,20 @@
 import React from "react";
-import { useLoaderData } from "react-router-dom";
 import useTitle from "../../hooks/useTitle";
 import Service from "../../components/Service";
+import { useQuery } from "@tanstack/react-query";
+import LoadingCircle from "../../components/ui/LoadingCircle";
 
 const Services = () => {
   useTitle("Services");
 
-  const services = useLoaderData();
+  const { data: services = [], isLoading } = useQuery({
+    queryKey: ["services"],
+    queryFn: () =>
+      fetch(`${import.meta.env.VITE_APP_SERVER}/services`)
+        .then(res => res.json())
+        .then(data => data)
+        .catch(err => console.error(err))
+  });
 
   return (
     <main>
@@ -14,6 +22,11 @@ const Services = () => {
         <h2 className="sm:text-3xl text-2xl font-medium title-font mb-4 py-4 text-center text-gray-900">
           My Services
         </h2>
+        {isLoading && (
+          <div className="mx-auto">
+            <LoadingCircle />
+          </div>
+        )}
         <div className="grid gap-8 lg:grid-cols-3">
           {services.map(service => (
             <Service key={service._id} service={service} />
